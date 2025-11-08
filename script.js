b1c2dc2ae629bff535670b211ae1426effe8e79f
Tayla Grobbelaar <tayla.grobbelaar@gmail.com>
2025-11-08 07:07:34 +0000

    Q2

---
+++ script.js
@@ -0,0 +1,13 @@
+//Q1
+let myString = "hello";
+let myBoolean = true;
+let myNumber = 42;
+
+//Q2
+15 > 10; // true, 15 is greater than 10
+10 === 10; // true, 10 is equal to 10
+15 < 20; // true, 15 is less than 20
+4 !== 5; // true, 4 is not equal to 5
+50 <= 50; // true, 50 is less than or equal to 50
+
+//Q3
