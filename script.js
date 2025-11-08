16dbf439ae2b8e949c792162978f74318dd2b342
Tayla Grobbelaar <tayla.grobbelaar@gmail.com>
2025-11-08 07:25:25 +0000

    questions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Q1
+/*Q1
 let myString = "hello";
 let myBoolean = true;
 let myNumber = 42;
@@ -11,3 +11,35 @@ let myNumber = 42;
 50 <= 50; // true, 50 is less than or equal to 50
 
 //Q3
+console.log('5 * 4 = ${5 * 4}');
+
+//Q4
+String(123);
+'${123}';
+
+//Q5
+(true && false) || !false;
+
+//Q6
+function isPositive(number) {
+    return number > 0;
+}
+
+//Q7
+for (let j = 1, j <= 10, j++)
+{
+    console.log(j);
+}
+let k = 1;
+while (k<=10)
+{
+    console.log(k);
+    k++;
+}*/
+
+//Q8
+let userNumber = prompt("Enter a number:");
+if (userNumber === 5 || userNumber === 13 || userNumber % 3 === 0)
+{
+    console.log("the number meets the criteria");
+}  
